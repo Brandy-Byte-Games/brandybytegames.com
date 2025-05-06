d42b053a34ebedaa5f23af29c9ffac2aceb682ad
Michael Cooper <mc454916@ohio.edu>
2025-05-05 22:17:43 -0400

    Material theme work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
